49bc56f3d00fb2149c2fb91313f6f1bebdaa962c
ricardonunosr <ricardonunosr@gmail.com>
2022-07-15 22:07:09 +0100

    add random route

---
+++ routes/random.tsx
@@ -0,0 +1,39 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Layout from "../components/layout.tsx";
+import { Head } from "https://deno.land/x/fresh@1.0.1/runtime.ts";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>ric</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Ricardo's random links"></meta>
+      </Head>
+      <h1 class={tw`font-bold`}>Good reads/ideas</h1>
+      <ul class={tw`list-disc ml-8`}>
+        <li>
+          <a
+            class={tw`hover:text-blue-500`}
+            href="https://users.ece.utexas.edu/~adnan/pike.html"
+          >
+            Rob Pike's Rules of Programming
+          </a>
+        </li>
+      </ul>
+      <h1 class={tw`font-bold`}>Useful links</h1>
+      <ul class={tw`list-disc ml-8`}>
+        <li>
+          <a
+            class={tw`hover:text-blue-500`}
+            href="https://casual-effects.com/data/"
+          >
+            McGuire Computer Graphics Archive
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  );
+}
